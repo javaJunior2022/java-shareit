237d8f59fb77730d6fa76b58d10c215374587ea5
s.timofeev <java.junior@icloud.com>
2022-09-20 11:10:25 +0300

    #8

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.item.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.user.model.User;
