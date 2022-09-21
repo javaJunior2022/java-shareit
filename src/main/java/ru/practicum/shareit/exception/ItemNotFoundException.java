24c593b0e4561e6dfaff705925d6ca19ecedf948
s.timofeev <java.junior@icloud.com>
2022-09-21 14:01:54 +0300

    #10

--- src/main/java/ru/practicum/shareit/exception/ItemNotFoundException.java
+++ src/main/java/ru/practicum/shareit/exception/ItemNotFoundException.java
@@ -2,6 +2,6 @@ package ru.practicum.shareit.exception;
 
 public class ItemNotFoundException extends RuntimeException {
     public ItemNotFoundException(long itemId) {
-        super(String.format("Can't find the item with this id %d"));
+        super(String.format("Can't find the item with this id %d",itemId));
     }
 }
